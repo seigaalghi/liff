cfe68dff52d75e478b293396979eb00987024320
seiga al ghifari <seigaalghi@gmail.com>
2020-12-04 20:15:17 +0700

    heroku

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { LiffProvider } from 'react-liff';
+
 import App from './App';
+
+const liffId = process.env.REACT_APP_LINE_LIFF_ID;
+const stubEnabled = process.env.NODE_ENV !== 'production';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LiffProvider liffId={liffId} stubEnabled={stubEnabled}>
+      <App />
+    </LiffProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
