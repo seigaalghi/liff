ce1e9c45c58c80c773a844f0690162274e731468
seiga al ghifari <seigaalghi@gmail.com>
2020-12-25 21:18:12 +0700

    fiuh

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ const App = () => {
   const [profile, setProfile] = useState({});
   const [isLogin, setisLogin] = useState(false);
   useEffect(() => {
-    window.liff.init({
-      liffId: '1655315643-O6DqdDE8',
-    });
+    window.liff
+      .init({
+        liffId: '1655315643-O6DqdDE8',
+      })
+      .then(() => {
+        if (isLogin) {
+          setProfile(window.liff.getProfile());
+        }
+      });
   }, []);
 
   const loginHandler = async () => {
