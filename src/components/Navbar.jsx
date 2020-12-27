ceb3d53d5ccf3ce6ce9fb20d8f36d8e7d290b7cd
seiga al ghifari <seigaalghi@gmail.com>
2020-12-27 16:41:55 +0700

    fc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <h1>Foody Ways</h1>
       <div className='action'>
         <div className='cart'>
-          <h4>{state.profile.displayName}</h4>
+          <h3>Hai {state.profile.displayName}</h3>
           <h4>Cart : {state.food.length + state.drink.length}</h4>
         </div>
         <img src={state.profile.pictureUrl} alt='avatar' />
--- src/context/context.js
+++ src/context/context.js
@@ -23,22 +23,22 @@ const reducer = (state, action) => {
         profile: payload,
       };
     case 'ADD_FOOD':
-      const checkFood = state.drink.find((drnk) => drnk.id === payload.id);
-      const newFood = checkFood ? { ...checkFood, count: checkFood.count + 1 } : payload;
+      const checkFood = state.food.find((fod) => fod.id === payload.id);
+      const newFood = checkFood ? { ...checkFood, count: checkFood.count + 1 } : null;
       return {
         ...state,
-        food: checkFood
+        food: newFood
           ? state.food.map((fod) => (fod.id === payload.id ? newFood : fod))
-          : [...state.food, newFood],
+          : [...state.food, payload],
       };
     case 'ADD_DRINK':
       const checkDrink = state.drink.find((drnk) => drnk.id === payload.id);
-      const newDrink = checkDrink ? { ...checkDrink, count: checkDrink.count + 1 } : payload;
+      const newDrink = checkDrink ? { ...checkDrink, count: checkDrink.count + 1 } : null;
       return {
         ...state,
-        drink: checkDrink
+        drink: newDrink
           ? state.drink.map((drnk) => (drnk.id === payload.id ? newDrink : drnk))
-          : [...state.drink, newDrink],
+          : [...state.drink, payload],
       };
     default:
       return state;
